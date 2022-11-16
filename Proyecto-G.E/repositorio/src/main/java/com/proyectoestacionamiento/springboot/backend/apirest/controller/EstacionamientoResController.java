660bc911945a8531bc761ac3bf75ca8a472bdbd1
TatyTatiana <taylok11@gmail.com>
2022-11-16 13:37:29 -0300

    Cliente, Estacionamiento, ServicioBano, ServicioEscaneo RestController

---
+++ Proyecto-G.E/repositorio/src/main/java/com/proyectoestacionamiento/springboot/backend/apirest/controller/EstacionamientoResController.java
@@ -0,0 +1,45 @@
+package com.proyectoestacionamiento.springboot.backend.apirest.controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.proyectoestacionamiento.springboot.backend.apirest.models.entity.Estacionamiento;
+import com.proyectoestacionamiento.springboot.backend.apirest.models.entity.ServicioBano;
+import com.proyectoestacionamiento.springboot.backend.apirest.service.IEstacionamientoService;
+
+@CrossOrigin(origins = {"http://localhost:4200"})
+@RestController
+@RequestMapping("/apiEstacionamiento")
+public class EstacionamientoResController {
+
+    @Autowired
+    IEstacionamientoService estacionamiento;
+
+    @GetMapping("/estacionamientos")
+    public ResponseEntity<?> index(){
+        Map<String,Object> response = new HashMap<>();
+        List<Estacionamiento> ListaEstacionamientos = null;
+        try {
+            ListaEstacionamientos = estacionamiento.findAll();
+            response.put("ok", true);
+        } catch (Exception e) {
+            // TODO: handle exception
+            response.put("mensaje", "Error al realizar la consulta en la base de datos");
+            response.put("ok", false);
+            return new ResponseEntity<Map<String,Object>>(response,HttpStatus.NOT_FOUND);
+        }
+        response.put("estacionamientos", ListaEstacionamientos);
+        return new ResponseEntity<Map<String,Object>>(response,HttpStatus.OK);
+
+    }
+
+}
