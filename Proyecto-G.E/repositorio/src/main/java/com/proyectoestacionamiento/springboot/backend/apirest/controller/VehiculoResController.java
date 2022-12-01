360b73793924918e9d7dc8d2e2cdeff6120ac605
Dresest <esteban.poblete.osses@gmail.com>
2022-12-01 16:20:26 -0300

    postVehiculo

--- Proyecto-G.E/repositorio/src/main/java/com/proyectoestacionamiento/springboot/backend/apirest/controller/VehiculoResController.java
+++ Proyecto-G.E/repositorio/src/main/java/com/proyectoestacionamiento/springboot/backend/apirest/controller/VehiculoResController.java
@@ -9,6 +9,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,5 +41,19 @@ public class VehiculoResController {
         response.put("vehiculos", vehiculos);
         return new ResponseEntity<Map<String,Object>>(response,HttpStatus.OK);
 
+    }
+    
+    @PostMapping("vehiculos")
+    public ResponseEntity<?> agregaVehiculo(@RequestBody Vehiculo vehiculo) {
+        Map<String, Object> response = new HashMap<>();
+        Vehiculo vehiculo2 = null;
+        try {
+            vehiculo2 = vehiculoService.save(vehiculo);
+            response.put("Ok", vehiculo2);
+        } catch (Exception e) {
+            response.put("mensaje", "Error al realizar la consulta en la base de datos");
+            return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_IMPLEMENTED);
+        }
+        return new ResponseEntity<Map<String, Object>>(response, HttpStatus.OK);
     }
 }
